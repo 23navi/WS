6a4f72293b06edf14616f1a26e5a55f5a987ef04
23navi <navisureka23@gmail.com>
2023-11-09 12:45:47 +0530

    client setup

---
+++ socket.io/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+app.use(express.static(__dirname + "/public"));
+const expressServer = app.listen(8000); // http server on port 8000
+const io = socketio(expressServer); // ws server on port 8000
