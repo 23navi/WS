2d989dc0872b7622b83ccc132babeec1be4e0e53
23navi <navisureka23@gmail.com>
2023-11-10 11:43:20 +0530

    more on connect and reconnect

--- socket.io/index.js
+++ socket.io/index.js
@@ -5,3 +5,8 @@ const app = express();
 app.use(express.static(__dirname + "/public"));
 const expressServer = app.listen(8000); // http server on port 8000
 const io = socketio(expressServer); // ws server on port 8000
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.emit("name", { data: "yoo" });
+});
