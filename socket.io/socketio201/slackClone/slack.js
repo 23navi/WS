7970dac56752fe7bfa2ac64990aa4d4afd3041f2
23navi <navisureka23@gmail.com>
2024-03-02 13:37:10 +0530

    emitWithAck

--- socket.io/socketio201/slackClone/slack.js
+++ socket.io/socketio201/slackClone/slack.js
@@ -31,8 +31,9 @@ io.on("connection", (socket) => {
 namespaces.forEach((ns) => {
   io.of(ns.endpoint).on("connect", (socket) => {
     // console.log(socket.id + " connnect to " + ns.name);
-    socket.on("joinRoom", ({ roomName, nsId }) => {
+    socket.on("joinRoom", ({ roomName, nsId }, cb) => {
       console.log({ roomName, nsId });
+      cb("value1", "value2");
     });
   });
 });
