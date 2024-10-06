a29cc6e901c447fa43f0034f97d7f6cafa3fd279
23navi <navisureka23@gmail.com>
2024-10-07 00:28:36 +0530

    update server

--- Trello/server/src/server.ts
+++ Trello/server/src/server.ts
@@ -1,19 +1,23 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import mongoose from "mongoose";
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  cors: {
-    origin: "*",
-  },
-});
+const io = new Server(httpServer);
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("API is UP");
 });
 
-httpServer.listen(3000, () => {
-  console.log("Server is running on port 3000");
+io.on("connection", () => {
+  console.log("connect");
+});
+
+mongoose.connect("mongodb://localhost:27017/eltrello").then(() => {
+  console.log("connected to mongodb");
+  httpServer.listen(4001, () => {
+    console.log(`API is listening on port 4001`);
+  });
 });
