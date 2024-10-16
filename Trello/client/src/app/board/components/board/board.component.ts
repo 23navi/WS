5aa1d074ae7e9ee9877b82a0fa220cf7e199af94
23navi <navisureka23@gmail.com>
2024-10-16 20:13:31 +0530

    auth

--- Trello/client/src/app/board/components/board/board.component.ts
+++ Trello/client/src/app/board/components/board/board.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, NavigationStart, Router } from '@angular/router';
+import { filter, Observable } from 'rxjs';
 import { BoardsService } from 'src/app/shared/services/boards.service';
+import { SocketService } from 'src/app/shared/services/socket.service';
+import { BoardInterface } from 'src/app/shared/types/board.interface';
+import { SocketEventsEnum } from 'src/app/shared/types/socketEvents.enum';
+import { BoardService } from '../../services/board.service';
 
 @Component({
   selector: 'board',
@@ -8,10 +13,14 @@ import { BoardsService } from 'src/app/shared/services/boards.service';
 })
 export class BoardComponent implements OnInit {
   boardId: string;
+  board$: Observable<BoardInterface>;
 
   constructor(
     private boardsService: BoardsService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router,
+    private boardService: BoardService,
+    private socketService: SocketService
   ) {
     const boardId = this.route.snapshot.paramMap.get('boardId');
 
@@ -20,15 +29,28 @@ export class BoardComponent implements OnInit {
     }
 
     this.boardId = boardId;
+    this.board$ = this.boardService.board$.pipe(filter(Boolean));
   }
 
   ngOnInit(): void {
+    this.socketService.emit(SocketEventsEnum.boardsJoin, {
+      boardId: this.boardId,
+    });
     this.fetchData();
+    this.initializeListeners();
+  }
+
+  initializeListeners(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.boardService.leaveBoard(this.boardId);
+      }
+    });
   }
 
   fetchData(): void {
     this.boardsService.getBoard(this.boardId).subscribe((board) => {
-      console.log('board', board);
+      this.boardService.setBoard(board);
     });
   }
 }
